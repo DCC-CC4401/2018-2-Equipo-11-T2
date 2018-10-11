a703ff56001583ea421eb67b573f38fc805f86e5
gonzalejandro <aege21@gmail.com>
2018-10-11 18:46:25 -0300

    deleted venv directory

--- T2/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/alejandro/Documents/2018-2-Tiburoncin-hu-ha-ha-T2/T2/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
